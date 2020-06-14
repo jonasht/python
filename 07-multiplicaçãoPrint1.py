74b5ea2643be795f4f63cd87085c2788ce1ae60f
jonasht <jhenriquet@outlook.com.br>
2020-06-13 23:04:56 -0300

    arrumado

--- 07-multiplicaçãoPrint1.py
+++ 07-multiplicaçãoPrint1.py
@@ -2,12 +2,12 @@ x1=[]
 x2=[]
 contar=0
 for i  in range(0, 11):
+    print('-'*12)
     for ii in range(0, 11):
+        x1.insert(contar, i)
+        x2.insert(contar, ii)
         contar = contar + 1
-
-op = input ('mostrar tabuada normal 1\nmostrar tabuada misturada 2\nop:')
-
-
-for iii in range(100):
-    print(f'{x1[i]} x {x2[i]}')
-    
+        print(i, 'X' ,ii, '=', i*ii)
+print('contar:', contar)
+for i in range(0, 121):
+    print(x1[i], 'x', x2[i])

68d6e06f69be1bfc18c283bf77a255faaa9f455c
jonasht <jhenriquet@outlook.com.br>
2020-06-16 23:54:57 -0300

    arrumando

--- 09-contador.py
+++ 09-contador.py
@@ -9,29 +9,6 @@ def l():
     print('=-=-'*15,'=')
 
 os.system('clear')
-
-
-for i in range(2, 10):
-    
-    x2.insert(i, i)
-print(x2)
-random.shuffle(x2)
-while 1:
-
-    op = int(input('\rwhich multiplication table would you like?, "e" to exit\nqual tabuada voce gostaria?, "s" para sair\nop:'))
-    
-    if str(op) == 's' or str(op) == 'e':
-        break
-
-    for i in x2:
-        l()
-        resposta = input(f'{op} X {i} = ')
-
-        os.system('clear')    
-        if resposta == op * i:
-            print(f'{op} X {i} = {resposta}\ncorreto\ncorrect')
-        else:
-            print(f'{op} X {i} = {resposta}\nincorreto\nincorrect')
-
-
-print('fim//the end')
+print('[', end='')
+for i in range (0, 10):
+    print(f'{i}, ', end='')

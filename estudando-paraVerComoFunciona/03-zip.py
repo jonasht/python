365d0819553485bab4d461073ce5235dada30fc0
jonasht <jhenriquet@outlook.com.br>
2020-09-27 11:47:33 -0300

    add

---
+++ estudando-paraVerComoFunciona/03-zip.py
@@ -0,0 +1,7 @@
+
+nomes = ['robson', 'fernando', 'lucio', 'sonia', 'fernanda', 'luiza']
+
+idades = [29, 29, 31, 21, 12, 21] 
+print()
+for nome, idade in zip(nomes, idades):
+    print(f'{nome.title()} tem {idade} anos')

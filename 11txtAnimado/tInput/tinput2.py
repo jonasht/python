15acb10bbc9c0d1cc1c310c56b465827fb22b8a3
jonasht <jhenriquet@outlook.com.br>
2020-10-08 23:05:37 -0300

    adding new code tinput

--- 11txtAnimado/tInput/tinput2.py
+++ 11txtAnimado/tInput/tinput2.py
@@ -7,32 +7,17 @@ f = '\33[m'
 bgBlue = '\033[44m' # backgournd blue
 bgYellow = '\033[43m' #backgournd yellow
 bgRed = '\033[41m' #backgournd red
-
-palavras=[]
-
-def tprint(p='', bg=bgBlue , fg=yellow, op=1):
-    
-    if op == 1 and p != '':
-        palavras.append(bg+fg+'  '+p+' '*2)
-    if op == 2:
-        palavras.append(bg+fg+'=-'*15+'=')
-    if op == 0:
-        print(end='')
-        for palavra in palavras:
-            print(f"{palavra:^5}{f}")
-        print(f, end='')
-        
+print('\n'*5)
 def tinput(mensagem, bg=bgBlue, fg=yellow):
-    entrada = input(bg + fg + mensagem + 3*" " + f)
+    fimDaMsg = '____________'
+    espaço = len(mensagem) * ' '
+
+    print(bg, fg, espaço + fimDaMsg + "\r" + mensagem, f, flush=True, end="")
+    entrada = input('') 
     return entrada
 
-print('\n')
-tprint(op=2, fg=green)
-tprint('qual opção voce deseja')
-tprint('1 - sacar')
-tprint('2 - depositar')
-tprint(op=2, fg=green)
-tprint(op=0)
-receber = tinput('qual opção:')
+
+receber = tinput('diga algo de bom para o mundo:')
+
 print(f'a opcão "{receber}" foi escolhida')
 print('\n')

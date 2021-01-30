e5846ca5c1c1fdb58940525cc23bf8650bd8c73e
jonasht <jhenriquet@outlook.com.br>
2021-01-30 19:05:14 -0300

    add

--- 15-programaDeAlgoritmo/7-programa/principal.py
+++ 15-programaDeAlgoritmo/7-programa/principal.py
@@ -2,7 +2,7 @@
 
 from algoritmo import *
 
-# diminuar fonto do terminal para poder ver
+# diminuar fonte do terminal para poder ver
 # M1(quantidade para o tamanho da lista, tempo de demora para atrasar)
 pr = M1(50, .01)
 pr.Maneira()

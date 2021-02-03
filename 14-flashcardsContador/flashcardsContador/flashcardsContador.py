1ac4cbfb82b06bcf2cf456ae45ac1e7a3086917a
jonasht <jhenriquet@outlook.com.br>
2021-02-03 17:35:49 -0300

    add

--- 14-flashcardsContador/flashcardsContador/flashcardsContador.py
+++ 14-flashcardsContador/flashcardsContador/flashcardsContador.py
@@ -5,21 +5,23 @@ recomeçar = Soma()
 começar = Soma()
 def mostrar():
     system('clear')
+    print('digite 0 para sair')
+    
     print('=-'*40+'=')
-    print(f'cartas para recomeçar: {recomeçar.Somar()}')
-    print(f'cartas para recomeçar: {começar.Somar()}')    
-    print(f'                total: {recomeçar.Somar()+começar.Somar()}')
+    print(f'|\tCartas para ReComeçar: {recomeçar.Somar()}')
+    print(f'|\tCartas para   Começar: {começar.Somar()}')    
+    print(f'|\t                Total: {recomeçar.Somar()+começar.Somar()}')
 
 
 while 1:
     mostrar()
-    n = input(f'n Para ReComeçar:')
+    n = input(f'|\tnumero Para ReComeçar:')
     if n == '0': break
     recomeçar.set_numeroDeCartas(n)
     
 while 1:
     mostrar()    
-    n = input(f'n Para Começar: ')
+    n = input(f'|\tnumero Para   Começar: ')
     if n == '0': break    
     começar.set_numeroDeCartas(n)
 

87afe55f8967e765fe58436540f982f76184aa86
jonasht <jhenriquet@outlook.com.br>
2021-03-08 20:57:05 -0300

    adicionando arqPOOVersao e rn o outro

--- ycons/1-primeiro/1-feitoSimples.py
+++ ycons/1-primeiro/1-feitoSimples.py
@@ -4,5 +4,5 @@ mochila = '3738,00'
 print(f'preços antigos: notebook:{notebook} mochila:{mochila} ')
 guardar = notebook
 notebook = mochila
-mochila = guardar
+mochila = guardar   
 print(f'preços atualizados: notebook:{notebook} mochila:{mochila}')

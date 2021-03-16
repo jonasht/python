98dc4eab377e25cc47cd6da433abbfeda7782c7c
jonasht <jhenriquet@outlook.com.br>
2021-03-15 22:11:55 -0300

    add

--- 16-calculoLinear3x3/4-3X3/cl_3x3.py
+++ 16-calculoLinear3x3/4-3X3/cl_3x3.py
@@ -1,58 +1,56 @@
 
-# calculo linear
+# calculo linear 
 # regra de cramer
+# --------------------------------------------------
 
 class cl_3x3:
-
-    def __init__(self, conta):
+    
+    def __init__(self,conta):
         self.conta = conta
         
-        self.matriz = [[], [], []]
-        
-    def mostrar_conta(self):
 
+    def mostrar_conta(self):
+        
         for dic in self.conta:
             for chave, item in dic.items():
                 if chave == '=':
-                    print(f' = {item}', end='')
+                    print(f' = {item}', end='')      
                 else:
                     if len(str(item)) == 1:
-                        print(f'  {item}{chave}', end='')
+                        print(f'  {item}{chave}', end='') 
                     else:
                         print(f' {item}{chave}', end='')
             print()
-            
 
-    def get_matrizMontada(self, opcao='='):
 
-        if opcao == '=':
-            for i, dic in enumerate(self.conta):
+    def montar_matriz(self, conta, op='='):
+        matriz = [[], [], []]
+        
+        if op == '=':
+            for i, dic in enumerate(conta):
                 for chave, item in dic.items():
                     if chave != '=':
-                        self.matriz[i].append(item)
-
+                        matriz[i].append(item)
+                        
         else:
-            for i, dic in enumerate(self.conta):
+            for i, dic in enumerate(conta):
                 for chave, item in dic.items():
-                    if chave != '=':
-                        if chave == opcao:
-                            self.matriz[i].append(self.conta[i]['='])
+                    if chave != '=':                
+                        if chave == op:
+                            matriz[i].append(conta[i]['='])
                         else:
-                            self.matriz[i].append(item)
-
+                            matriz[i].append(item)                    
+                            
         for i in range(3):
             for ii in range(2):
-                self.matriz[i].append(self.matriz[i][ii])
+                matriz[i].append(matriz[i][ii])
+        return matriz
+
+
+
+    def mostrar_matriz(self, matriz):
         
-
-        return self.matriz
-
-
-    
-    def mostrar_matriz(self):
-
-        
-        for lista in self.matriz:
+        for lista in matriz:
             for n in lista:
                 if len(str(n)) == 1:
                     print(f'  {n}', end='')
@@ -61,21 +59,21 @@ class cl_3x3:
             print()
 
 
-    def somarMatriz(self):
+    def somarMatriz(self, matriz):
         
         multiplicacao = 1
         resultado = 0
         for seguinte in range(3):
             for i in range(3):
-                multiplicacao *= self.matriz[i][i+seguinte]
+                multiplicacao *= matriz[i][i+seguinte]
             resultado += multiplicacao
             multiplicacao = 1
-
+                
         for seguinte in range(3):
-            for i in range(3):
-                multiplicacao *= -(self.matriz[-(i-2)][i+seguinte])
+            for i in range(3):            
+                multiplicacao *= -(matriz[-(i-2)][i+seguinte])
             resultado += multiplicacao
             multiplicacao = 1
 
+            
         return resultado
-
--- 16-calculoLinear3x3/4-3X3/principal.py
+++ 16-calculoLinear3x3/4-3X3/principal.py
@@ -6,49 +6,53 @@ conta = [
     {'x': 3, 'y': 1, 'z':-1, '=': 2}
 ]
 # --------------------------------------------------
-print()
+def enfeitar():
+    print('-' * 30)
 
 a = cl_3x3(conta)
 
+print()
 
-
+enfeitar()
 print('conta:')
-
 a.mostrar_conta()
 
-#print('lista delta:')
-#
-#lista_delta = montar_matriz(conta)
-#mostrar_matriz(lista_delta)
-#
-#
-#delta = somarMatriz(lista_delta)
-#print(f'delta = {delta}')
-#
-#
-#lista_deltaX = montar_matriz(conta, 'x')
-#mostrar_matriz(lista_deltaX)
-#deltaX = somarMatriz(lista_deltaX)
-#print(f'deltaX = {deltaX}')
-#
-#matriz_deltaY = montar_matriz(conta, 'y')
-#mostrar_matriz(matriz_deltaY)
-#
-#deltaY = somarMatriz(matriz_deltaY)
-#print(f'deltaX = {deltaY}')
-#
-#enfeitar()
-#matriz_deltaZ = montar_matriz(conta, 'z')
-#mostrar_matriz(matriz_deltaZ)
-#
-#deltaZ = somarMatriz(matriz_deltaZ)
-#print(f'deltaZ = {deltaZ}')
-#
-#enfeitar()
-#print(f'delta={delta}, deltaX={deltaX}, deltaY={deltaY}, deltaZ={deltaZ}')
-#
-#print(f'x = {deltaX}/{delta} = {int(deltaX/delta)}')
-#print(f'y = {deltaY}/{delta} = {int(deltaY/delta)}')
-#print(f'z = {deltaZ}/{delta} = {int(deltaZ/delta)}')
-#enfeitar()
-#print(f'x = {int(deltaX/delta)}, y = {int(deltaY/delta)}, z = {int(deltaZ/delta)}')
+enfeitar()
+print('lista delta:')
+lista_delta = a.montar_matriz(conta)
+a.mostrar_matriz(lista_delta)
+
+
+delta = a.somarMatriz(lista_delta)
+
+print(f'delta = {delta}')
+enfeitar()
+
+lista_deltaX = a.montar_matriz(conta,'x')
+a.mostrar_matriz(lista_deltaX)
+deltaX = a.somarMatriz(lista_deltaX)
+print(f'deltaX = {deltaX}')
+
+enfeitar()
+
+matriz_deltaY = a.montar_matriz(conta,'y')
+a.mostrar_matriz(matriz_deltaY)
+
+deltaY = a.somarMatriz(matriz_deltaY)
+print(f'deltaX = {deltaY}')
+
+enfeitar()
+matriz_deltaZ = a.montar_matriz(conta, 'z')
+a.mostrar_matriz(matriz_deltaZ)
+
+deltaZ = a.somarMatriz(matriz_deltaZ)
+print(f'deltaZ = {deltaZ}')
+
+enfeitar()
+print(f'delta={delta}, deltaX={deltaX}, deltaY={deltaY}, deltaZ={deltaZ}')
+
+print(f'x = {deltaX}/{delta} = {int(deltaX/delta)}')
+print(f'y = {deltaY}/{delta} = {int(deltaY/delta)}')
+print(f'z = {deltaZ}/{delta} = {int(deltaZ/delta)}')
+enfeitar()
+print(f'x = {int(deltaX/delta)}, y = {int(deltaY/delta)}, z = {int(deltaZ/delta)}')

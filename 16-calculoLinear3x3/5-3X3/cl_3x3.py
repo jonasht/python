7dc7bf059d9e4dd96ba92cffd030d26301ffdc1f
jonasht <jhenriquet@outlook.com.br>
2021-03-15 23:09:51 -0300

    add

---
+++ 16-calculoLinear3x3/5-3X3/cl_3x3.py
@@ -0,0 +1,79 @@
+
+# calculo linear 3x3
+# regra de cramer
+# --------------------------------------------------
+
+class cl_3x3:
+    
+    def __init__(self,conta):
+        self.conta = conta
+        
+
+    def mostrar_conta(self):
+        
+        for dic in self.conta:
+            for chave, item in dic.items():
+                if chave == '=':
+                    print(f' = {item}', end='')      
+                else:
+                    if len(str(item)) == 1:
+                        print(f'  {item}{chave}', end='') 
+                    else:
+                        print(f' {item}{chave}', end='')
+            print()
+
+
+    def get_matrizMontada(self, op='='):
+        matriz = [[], [], []]
+        
+        if op == '=':
+            for i, dic in enumerate(self.conta):
+                for chave, item in dic.items():
+                    if chave != '=':
+                        matriz[i].append(item)
+                        
+        else:
+            for i, dic in enumerate(self.conta):
+                for chave, item in dic.items():
+                    if chave != '=':                
+                        if chave == op:
+                            matriz[i].append(self.conta[i]['='])
+                        else:
+                            matriz[i].append(item)                    
+                            
+        for i in range(3):
+            for ii in range(2):
+                matriz[i].append(matriz[i][ii])
+        return matriz
+
+
+
+    def mostrar_matriz(self, matriz):
+        
+        for lista in matriz:
+            for n in lista:
+                if len(str(n)) == 1:
+                    print(f'  {n}', end='')
+                else:
+                    print(f' {n}', end='')
+            print()
+
+
+    def somarMatriz(self, matriz):
+        
+        multiplicacao = 1
+        resultado = 0
+        for seguinte in range(3):
+            for i in range(3):
+                multiplicacao *= matriz[i][i+seguinte]
+            resultado += multiplicacao
+            multiplicacao = 1
+                
+        for seguinte in range(3):
+            for i in range(3):            
+                multiplicacao *= -(matriz[-(i-2)][i+seguinte])
+            resultado += multiplicacao
+            multiplicacao = 1
+
+            
+        return resultado
---
+++ 16-calculoLinear3x3/5-3X3/principal.py
@@ -0,0 +1,60 @@
+from cl_3x3 import *
+# --------------------------------------------------
+conta = [
+    {'x': 1, 'y': 2, 'z': 1, '=': 8},
+    {'x': 2, 'y': -1, 'z': 1, '=': 3},
+    {'x': 3, 'y': 1, 'z': -1, '=': 2}
+]
+# --------------------------------------------------
+
+
+def enfeitar():
+    print('-' * 30)
+
+
+a = cl_3x3(conta)
+
+print()
+
+enfeitar()
+print('conta:')
+a.mostrar_conta()
+
+enfeitar()
+print('lista delta:')
+lista_delta = a.get_matrizMontada()
+a.mostrar_matriz(lista_delta)
+
+
+delta = a.somarMatriz(lista_delta)
+
+print(f'delta = {delta}')
+enfeitar()
+
+lista_deltaX = a.get_matrizMontada('x')
+a.mostrar_matriz(lista_deltaX)
+deltaX = a.somarMatriz(lista_deltaX)
+print(f'deltaX = {deltaX}')
+
+enfeitar()
+matriz_deltaY = a.get_matrizMontada('y')
+a.mostrar_matriz(matriz_deltaY)
+
+deltaY = a.somarMatriz(matriz_deltaY)
+print(f'deltaX = {deltaY}')
+
+enfeitar()
+matriz_deltaZ = a.get_matrizMontada('z')
+a.mostrar_matriz(matriz_deltaZ)
+
+deltaZ = a.somarMatriz(matriz_deltaZ)
+print(f'deltaZ = {deltaZ}')
+
+enfeitar()
+print(f'delta={delta}, deltaX={deltaX}, deltaY={deltaY}, deltaZ={deltaZ}')
+
+print(f'x = {deltaX}/{delta} = {int(deltaX/delta)}')
+print(f'y = {deltaY}/{delta} = {int(deltaY/delta)}')
+print(f'z = {deltaZ}/{delta} = {int(deltaZ/delta)}')
+enfeitar()
+print(f'x = {int(deltaX/delta)}, y = {int(deltaY/delta)}, z = {int(deltaZ/delta)}')

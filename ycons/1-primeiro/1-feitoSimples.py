9978c01160d8f3017ee2c7e71662067eb970b499
jonasht <jhenriquet@outlook.com.br>
2021-03-27 12:59:46 -0300

    rm ycons

--- ycons/1-primeiro/1-feitoSimples.py
+++
@@ -1,8 +0,0 @@
-
-notebook = '137,00'
-mochila = '3738,00'
-print(f'preços antigos: notebook:{notebook} mochila:{mochila} ')
-guardar = notebook
-notebook = mochila
-mochila = guardar   
-print(f'preços atualizados: notebook:{notebook} mochila:{mochila}')

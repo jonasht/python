12cf061aea3fa376353024a3f8a9dbff9ca68f5f
jonasht <jhenriquet@outlook.com.br>
2021-04-04 09:45:34 -0300

    adding auroDesenhante new version

--- 11-txtAnimado-auroTUI/0auroDesenhante/5-emDev.py
+++ 11-txtAnimado-auroTUI/0auroDesenhante/5-emDev.py
@@ -6,20 +6,36 @@ class auro:
         self.altura = altura
         self.encher = encher
         self.desenho = [[self.encher for ii in range(self.largura)] for i in range(self.altura)]
+        self.posicao = {}
     
     
     def desenhar(self, comOque, x=2, y=2, qtdCasas=2, ):
         for i in range(qtdCasas):
             self.desenho[y][x+i] = comOque
-    
+        posicao = {'nome': comOque, 'x': x, 'y': y, 'qtdCasas': qtdCasas}
+        self.set_posicao(posicao)
+
+        
     def mostrar(self):
         for chars in self.desenho:
             for char in chars:
                 print(f'{char}', end='')
             print()
+
     def apagar(self, x, y, qtdCasas):
         for i in range(qtdCasas):
             self.desenho[y][x+i] = self.encher
+            
+    def set_posicao(self, p): 
+        # p é de posicao
+       self.posicao[p['nome']] = {
+           'nome': p['nome'], 
+           'x': p['x'], 
+           'y': p['y'],
+           'qtdCasas': p['qtdCasas']}
+    
+    def get_posicao(self):
+        return self.posicao
             
 fim = '\033[0m'
 black = '\033[40m'
@@ -31,10 +47,11 @@ pink = '\033[45m'
 white = '\033[107m'
 
 def magica():
-    sleep(0.5)
+    # sleep(0.5)
+    sleep(.1)
     system('clear')
 
-a = auro(20, 10, f'{black}  {fim}')
+a = auro(40, 10, f'{black}  {fim}')
 
 a.mostrar()
 print('----------------------------------------')
@@ -56,3 +73,13 @@ a.mostrar()
 a.apagar(x=7, y=3, qtdCasas=5)
 magica()
 a.mostrar()
+
+print('-------------------------------------------------------------')
+print('posicao')
+print(a.get_posicao())
+# mostrar possicao de modo melhor
+for nome, posicao in a.get_posicao().items():
+    print(f'nomePrimario: {nome} - ', end='')
+    for n, p in posicao.items():
+        print(f'{n}:{p} ', end='')
+    print()

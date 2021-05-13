0fed5f34b81fb6c20d54bfdd5a62dc883735d7df
jonasht <jhenriquet@outlook.com.br>
2021-05-13 13:53:08 -0300

    add editando cgi basico

--- estudando-paraVerComoFunciona/cgi-basico/1cgi.py
+++ estudando-paraVerComoFunciona/cgi-basico/1cgi.py
@@ -2,8 +2,11 @@
 import cgi
 form = cgi.FieldStorage()
 print('Content-type: text/html\n')
-print('<title>Reply Page</title>')
+print('<title>Pagina de resposta</title>')
 if not 'user' in form:
+    print('<hr>')
+    print('=-=-=-=-=-=-=-=-=-=-=-=')
+    print('<hr>')
     print('<h1>quem eh voce?</h1>')
 else:
     print('<h1>oi <i>%s</i>!</h1>' % cgi.escape(form['user'].value))

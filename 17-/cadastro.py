87f6a5a0c7407b6a939397d735c75e84e97dd746
jonasht <jhenriquet@outlook.com.br>
2021-05-20 17:42:49 -0300

    add

--- 17-/cadastro.py
+++ 17-/cadastro.py
@@ -8,43 +8,48 @@ def cadastrar():
         sexo=entdSexo.get(),
         idade=entdIdade.get()
     )
+    lb_aviso.config(text='cadastro feito com sucesso', fg='green')
     
     print(bd)
-# cadastro.config(background='gray')
+cadastro.geometry('200x150+400+300')
 
 # id
 lb_id = Label(cadastro, text='id:')
 entd_id = Entry(cadastro)
-lb_id.grid(row=0, column=1)
-entd_id.grid(row=0, column=2)
+lb_id.grid(row=0, column=1, pady=2)
+entd_id.grid(row=0, column=2, pady=2)
 
 lbnome = Label(cadastro, text='nome:')
-lbnome.grid(row=1, column=1)
+lbnome.grid(row=1, column=1, pady=2)
 cadastro.title('cadastro')
 
 # nome
 entdNome = Entry(cadastro)
-entdNome.grid(row=1, column=2)
+entdNome.grid(row=1, column=2, pady=2)
 
 # sexo
 lbsexo = Label(cadastro, text='sexo:')
 entdSexo = Entry(cadastro)
-lbsexo.grid(row=2, column=1)
-entdSexo.grid(row=2, column=2)
+lbsexo.grid(row=2, column=1, pady=2)
+entdSexo.grid(row=2, column=2, pady=2)
 
 # idade
 lbidade = Label(cadastro, text='idade:')
 entdIdade = Entry(cadastro)
-lbidade.grid(row=3, column=1)
-entdIdade.grid(row=3, column=2)
+lbidade.grid(row=3, column=1, pady=2)
+entdIdade.grid(row=3, column=2, pady=2)
+
+# label de aviso
+lb_aviso = Label(cadastro, text='   ')
+lb_aviso.grid(row=4, column=1, columnspan=3, padx=2, pady=2, sticky='news')
 
 # botao cadatrar
 btcadastrar = Button(cadastro, text='cadastrar', command=cadastrar)
-btcadastrar.grid(row=4, column=2, columnspan=3)
+btcadastrar.grid(row=5, column=2, columnspan=3, padx=2, pady=2, sticky='news')
 
 # botao sair
 btsair = Button(cadastro, text='sair', command=cadastro.quit)
-btsair.grid(row=4, column=1)
+btsair.grid(row=5, column=1, pady=2, padx=2, sticky='news')
 
 
 cadastro.mainloop()
---
+++ 17-/telaPrincipal.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+def cadastrar():
+    print("entrando em cadastro")
+def conta():
+    print('entrando em conta')
+    
+root = Tk()
+root.geometry('200x150+400+300')
+lb_telaPricipal = Label(root, text='tela principal', fg='blue').pack(pady=2)
+bt_cadastrar = Button(root, text='cadastrar', width=15, command=cadastrar).pack(pady=2)
+bt_conta     = Button(root, text='conta', width=15, command=conta).pack(pady=2)
+root.mainloop()

92a4ba3d402ac797136f83549b7ae76dfa80765f
jonasht <jhenriquet@outlook.com.br>
2021-06-01 20:38:13 -0300

    add

--- 04-AprendizagemDeTabuada/tabuada-V5-EmDev/frameMenu.py
+++ 04-AprendizagemDeTabuada/tabuada-V5-EmDev/frameMenu.py
@@ -1,20 +1,14 @@
 from tkinter import *
 from interfaceStart import FrameStart
-from sys import builtin_module_names, exit
-
-print('feito no gnu/linux, fedora 34')
 
 
-class Tabuada(Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('FTabuada v4')
-        self.geometry('500x400')
+class Menu(Frame):
+    def __init__(self, container):
+        super().__init__(container)
         
         self.opcaoMenu = Frame(self)
     
         self.bt_start = Button(self, text='start', command=self.Start, font='arial 20')
-        self.bind('<Return>', self.teclaEnter)
         self.valor = IntVar()
 
         self.lb_op = Label(self.opcaoMenu, text='qual tabuada?:')
@@ -46,87 +40,35 @@ class Tabuada(Tk):
         self.rbt10.pack(side=LEFT)
 
         self.rbt9.select()
-        # ========================================================
         
-        # teclado dos numeros das tabuadas
-        self.bind('1', self.tecla1)
-        self.bind('2', self.tecla2)
-        self.bind('3', self.tecla3)
-        self.bind('4', self.tecla4)
-        self.bind('5', self.tecla5)
-        self.bind('6', self.tecla6)
-        self.bind('7', self.tecla7)
-        self.bind('8', self.tecla8)
-        self.bind('9', self.tecla9)
-        
-        self.frameVoltarSair = Frame(self)
-        self.bt_voltar = Button(self.frameVoltarSair, 
-                                text='Voltar', width=15, font='arial 20 bold', 
-                                command=self.esconderFrameStart)
-        self.bt_sair = Button(self.frameVoltarSair, 
-                              text='Sair', width=20, 
-                              font='arial 20 bold',
-                              command=exit
-                              )
-
-        self.bt_voltar.pack(side=RIGHT)
-        self.bt_sair.pack(side=LEFT)
-
-        self.frameVoltarSair.pack(side=TOP) 
-    
-        # mostrar menu - chamando a funcao
         self.mostrarMenu()
+        
 
-    # funcoes de aperto de teclas
-    def tecla1(self, event):
-        self.rbt1.select()
-    def tecla2(self, event):
-        self.rbt2.select()
-    def tecla3(self, event):
-        self.rbt3.select()
-    def tecla4(self, event):
-        self.rbt4.select()
-    def tecla5(self, event):
-        self.rbt5.select()
-    def tecla6(self, event):
-        self.rbt6.select()
-    def tecla7(self, event):
-        self.rbt7.select()
-    def tecla8(self, event):
-        self.rbt8.select()
-    def tecla9(self, event):
-        self.rbt9.select()
-        
-        
-    def teclaEnter(self, event):
-        self.Start()
-        
-    def esconder(self):
-        self.opcaoMenu.pack_forget()
-        self.bt_start.pack_forget()
-        
-    def esconderFrameStart(self):
-        self.frameStart.pack_forget()
-        self.mostrarMenu()
 
     def mostrarMenu(self): 
-        self.opcaoMenu.pack()
         self.opcaoMenu.pack(anchor=CENTER, padx=10, pady=20)
         self.bt_start.pack(anchor=CENTER)
-        self.bt_voltar.config(state=DISABLED)
+
     
     
     def Start(self):  
-        self.esconder()
-        self.bt_voltar.config(state=NORMAL)
-        
         self.frameStart = FrameStart(self)
         self.frameStart.pack()
         self.frameStart.iniciar(self.valor.get())
 
 
-
-
 if __name__ == '__main__':
-    root = Tabuada()
+    root = Tk()
+    frame = Menu(root)
+    def tecla1(event):
+        print('1 apertado')
+        frame.rbt1.select()
+    def tecla2(event):
+        print('-2')
+        frame.rbt2.select()
+
+    root.bind('1', tecla1)
+    root.bind('2', tecla2)
+    frame.pack()
+    
     root.mainloop()
---
+++ 04-AprendizagemDeTabuada/tabuada-V5-EmDev/main.py
@@ -0,0 +1,94 @@
+from tkinter import *
+from interfaceStart import FrameStart
+from frameMenu import Menu
+
+class Principal(Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title('FTabuada v5')
+        self.geometry('500x400')
+        
+        # chamando classes
+        
+        self.menu = Menu(self)
+        self.frameStart = FrameStart(self)
+                
+        # teclado dos numeros das tabuadas
+        self.bind('1', self.tecla1)
+        self.bind('2', self.tecla2)
+        self.bind('3', self.tecla3)
+        self.bind('4', self.tecla4)
+        self.bind('5', self.tecla5)
+        self.bind('6', self.tecla6)
+        self.bind('7', self.tecla7)
+        self.bind('8', self.tecla8)
+        self.bind('9', self.tecla9)
+        
+        # tecla enter
+        self.bind('<Return>', self.teclaEnter)
+        # botoes do topo da tela ================================
+        self.frameVoltarSair = Frame(self)
+        
+        self.bt_voltar = Button(self.frameVoltarSair, 
+                                text='Voltar', width=15, font='arial 20 bold', 
+                                command=self.esconderFrameStart)
+        self.bt_sair = Button(self.frameVoltarSair, 
+                              text='Sair', width=20, 
+                              font='arial 20 bold',
+                              command=exit
+                              )
+
+        self.bt_voltar.pack(side=RIGHT)
+        self.bt_sair.pack(side=LEFT)
+
+        self.frameVoltarSair.pack(side=TOP) 
+        
+        self.mostrarMenu()
+        
+    def tecla1(self, event):
+        self.menu.rbt1.select()
+    def tecla2(self, event):
+        self.menu.rbt2.select()
+    def tecla3(self, event):
+        self.menu.rbt3.select()
+    def tecla4(self, event):
+        self.menu.rbt4.select()
+    def tecla5(self, event):
+        self.menu.rbt5.select()
+    def tecla6(self, event):
+        self.menu.rbt6.select()
+    def tecla7(self, event):
+        self.menu.rbt7.select()
+    def tecla8(self, event):
+        self.menu.rbt8.select()
+    def tecla9(self, event):
+        self.menu.rbt9.select()
+    
+    def teclaEnter(self, event):
+        self.Start()
+        
+    def esconderMenu(self):
+        self.menu.pack_forget()
+        
+    def esconderFrameStart(self):
+        self.frameStart.pack_forget()
+        self.mostrarMenu()
+
+    def mostrarMenu(self): 
+        self.menu.pack()
+        self.bt_voltar.config(state=DISABLED)   
+    
+    def Start(self):  
+        self.esconderMenu()
+        
+        self.bt_voltar.config(state=NORMAL)
+
+        self.frameStart.pack()
+        self.frameStart.iniciar(self.valor.get())
+
+
+
+    
+root = Principal()
+root.mainloop()

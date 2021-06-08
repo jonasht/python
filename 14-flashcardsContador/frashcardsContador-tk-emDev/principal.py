4ee2f89a9ef5666e74b39bc00ad9595da1afbfd6
jonasht <jhenriquet@outlook.com.br>
2021-06-08 18:22:51 -0300

    add ultimas versoes dev

--- 14-flashcardsContador/frashcardsContador-tk-emDev/principal.py
+++ 14-flashcardsContador/frashcardsContador-tk-emDev/principal.py
@@ -22,101 +22,202 @@ class Principal(Tk):
         self.bind('8', self.botao8)
         self.bind('9', self.botao9)
         
-        # tecla enter
+        # teclado numPad / teclado do lado do teclado
+        self.bind('<KP_1>', self.teclaNum1)
+        self.bind('<KP_2>', self.teclaNum2)
+        self.bind('<KP_3>', self.teclaNum3)
+        self.bind('<KP_4>', self.teclaNum4)
+        self.bind('<KP_5>', self.teclaNum5)
+        self.bind('<KP_6>', self.teclaNum6)
+        self.bind('<KP_7>', self.teclaNum7)
+        self.bind('<KP_8>', self.teclaNum8)
+        self.bind('<KP_9>', self.teclaNum9)
+        
+        # tecla enter e tecla enter NumPad
         self.bind('<Return>', self.teclaEnter)
+        self.bind('<KP_Enter>', self.teclaNumEnter)
+        # ====================================
         
         # tecla backspace/apagar
         self.bind('<BackSpace>', self.apagar)
-        self.recomecar = True
         
-        self.mudarDeCor()
+        self.bind('<Up>', self.teclaUp)
+        self.bind('<Down>', self.teclaDown)
+
+        # self.posicaofileira[0] = True
+        self.posicaofileira = [True, False]
+        self.mostrarFileira()
         self.mostrar()
 
     def botao1(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(1)
         else:
             self.conta.somarComecar(1)
             
         self.mostrar()
-        print('funcionando 1')
+
         
     def botao2(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(2)
         else:
             self.conta.somarComecar(2)
         self.mostrar()
         
     def botao3(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(3)
         else:
             self.conta.somarComecar(3)
         self.mostrar()
         
     def botao4(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(4)
         else:
             self.conta.somarComecar(4)
         self.mostrar()
         
     def botao5(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(5)
         else:
             self.conta.somarComecar(5)
         self.mostrar()
         
     def botao6(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(6)
         else:
             self.conta.somarComecar(6)
         self.mostrar()
     def botao7(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(7)
         else:
             self.conta.somarComecar(7)
         self.mostrar()
         
     def botao8(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(8)
         else:
             self.conta.somarComecar(8)
         self.mostrar()
         
     def botao9(self, event):
-        if self.recomecar:
+        if self.posicaofileira[0]:
             self.conta.somarRecomecar(9)
         else:
             self.conta.somarComecar(9)
         self.mostrar()
+    
+    # funcoes das teclado numerica NumPad/ teclas numericas do lado do teclado
+    def teclaNum1(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(1)
+        else:
+            self.conta.somarComecar(1)
+        self.mostrar()
+
+    def teclaNum2(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(2)
+        else:
+            self.conta.somarComecar(2)
+        self.mostrar()
+
+    def teclaNum3(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(3)
+        else:
+            self.conta.somarComecar(3)
+        self.mostrar()
+
+    def teclaNum4(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(4)
+        else:
+            self.conta.somarComecar(4)
+        self.mostrar()
+
+    def teclaNum5(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(5)
+        else:
+            self.conta.somarComecar(5)
+        self.mostrar()
+
+
+    def teclaNum6(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(6)
+        else:
+            self.conta.somarComecar(6)
+        self.mostrar()
+
+    def teclaNum7(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(7)
+        else:
+            self.conta.somarComecar(7)
+        self.mostrar()
+
+    def teclaNum8(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(8)
+        else:
+            self.conta.somarComecar(8)
+        self.mostrar()
+
+    def teclaNum9(self, event):
+        if self.posicaofileira[0]:
+            self.conta.somarRecomecar(9)
+        else:
+            self.conta.somarComecar(9)
+        self.mostrar()
+
+        
     def teclaEnter(self, event):
-        self.recomecar = False
+        self.posicaofileira.reverse()
         print('enter apertado')
-        self.mudarDeCor(2)
+        self.mostrarFileira()
+    def teclaNumEnter(self, event):
+        self.posicaofileira.reverse()
+        print('enter apertado')
+        self.mostrarFileira()
+        
+    def teclaUp(self, event):
+        print('up')
+        self.posicaofileira.reverse()
+        self.mostrarFileira()
+        
+    def teclaDown(self, event):
+        print('down')
+        self.posicaofileira.reverse()
+        self.mostrarFileira()        
         
     # backspace/apagar ultimo item de lista
     def apagar(self, event):
-        if self.recomecar and self.conta.recomecar:
+        if self.posicaofileira[0] and self.conta.recomecar:
             del(self.conta.recomecar[-1])
-        elif self.conta.comecar:
+        if self.posicaofileira[1] and self.conta.comecar:
             del(self.conta.comecar[-1])
         
         self.mostrar()
-        print('funcionando')
-    # mudar de cor as rows da frames ...
-    def mudarDeCor(self, op=1):
-        if op==1:
+        
+        
+    # mudar rows da frames ...
+    def mostrarFileira(self):
+        if self.posicaofileira[0]:
             self.frame.lb2.config(bg='lightgray')
             self.frame.lb_numero2.config(bg='lightgray')
+            
             self.frame.lb1.config(bg='gray')
             self.frame.lb_numero1.config(bg='gray')
-        if op==2:
+
+        if self.posicaofileira[1]:
             self.frame.lb1.config(bg='lightgray')
             self.frame.lb_numero1.config(bg='lightgray')    
                     

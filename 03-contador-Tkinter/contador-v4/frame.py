1fcdef068cca7ec1e731481f5239116b59b7a59f
jonasht <jhenriquet@outlook.com.br>
2021-06-15 14:31:16 -0300

    adicionando nova versao

--- 03-contador-Tkinter/contador-v4/frame.py
+++ 03-contador-Tkinter/contador-v4/frame.py
@@ -1,50 +1,57 @@
 from tkinter import *
-from time import sleep
+
 
 class Interface(Frame):
     def __init__(self, parent):
         super().__init__(parent)
         self['bg']='Black'
+        self.frame_principal = Frame(self, bg='black')
         
-        self.label = Label(self, text="quantos segundos:", fg='green', bg='black')
+        self.label = Label(self.frame_principal, text="quantos segundos:", fg='green', bg='black')
 
-        self.entrada = Entry(self, textvariable=0, borderwidth=5, bg='gray')
+        self.entrada = Entry(self.frame_principal, textvariable=0, borderwidth=5, bg='gray')
 
-        self.lb_contagem = Label(self, fg='green', font='Times 100 bold', bg='black', text='0')
+        self.lb_contagem = Label(self.frame_principal, fg='green', font='Times 100 bold', bg='black', text='0')
 
-        self.bt = Button(self, fg='dark green', bg='light sea green', padx=25, pady=25,
+        self.bt = Button(self.frame_principal, fg='dark green', bg='gray', padx=25, pady=25,
                     text='Começar',
                     command=self.Contagem,
                     font='Arial 20 bold')
 
-        self.bt_sair = Button(self, text='sair', fg='dark green', bg='light sea green', command=self.quit, font='Arial 20 bold')
+        self.bt_sair = Button(self.frame_principal, text='sair', fg='dark green', bg='gray', command=self.quit, font='Arial 20 bold')
+        
         self.label.grid(row=0, column=0)
         self.entrada.grid(row=0, column=1)
-        self.lb_contagem.grid(row=2, column=0, columnspan=2, sticky=W+E)
+        self.lb_contagem.grid(row=2, column=0, columnspan=2, sticky=W+E, pady=30)
         self.bt.grid(row=3, column=0, columnspan=2, sticky=W+E)
         self.bt_sair.grid(row=4, columnspan=2, sticky=W+E)
 
-
-
         
+        self.frame_principal.pack()
+
+        self.entrada.focus()
         self.ate=0
         self.segundos = None
 
     def Contagem(self):
+        self.bt.config(state=DISABLED)
         
         if self.segundos == None:
             if self.entrada.get() != '':
                 self.ate = int(self.entrada.get())
             else:
+                self.entrada.delete(0, END)
+                self.entrada.insert(0, 5)
                 self.ate = 5
                 
             self.segundos = -1 
         if self.segundos == self.ate:
             self.lb_contagem['text'] = 'Fim'
             
+            
             self.segundos = None
             self.ate = 0
-
+            self.bt.config(state=ACTIVE)
         else:
             self.segundos += 1
             self.lb_contagem['text'] = self.segundos

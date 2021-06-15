99f463ca9a4ecfccca3dac5af2c990466313a237
jonasht <jhenriquet@outlook.com.br>
2021-06-15 14:10:24 -0300

    add

---
+++ 03-contador-Tkinter/contador-v3-EmDev/main.py
@@ -0,0 +1,27 @@
+from tkinter import Tk
+from tkinter.constants import DISABLED
+from frame import Interface
+
+class Principal(Tk):
+    def __init__(self):
+        super().__init__()
+        self.frame = Interface(self)
+        self.frame.pack()
+        
+        self.bind('<Return>', self.teclaEnter)
+        
+        self.frame.entrada.focus()
+        self.frame.bt.config(state=)
+        
+    def teclaEnter(self, event):
+        self.iniciar()
+    def iniciar(self):
+        self.frame.Contagem()
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    root = Principal()
+    root.mainloop()

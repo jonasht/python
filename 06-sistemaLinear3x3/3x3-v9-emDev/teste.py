65a8408cc32742dd81d4d99a84089f558bed94f2
jonasht <jhenriquet@outlook.com.br>
2021-06-21 08:28:14 -0300

    progredindo para arrumar os bugs

--- 06-sistemaLinear3x3/3x3-v9-emDev/teste.py
+++ 06-sistemaLinear3x3/3x3-v9-emDev/teste.py
@@ -1,47 +1,22 @@
 from uteis import *
 
 
-def filtrar(chave, valor):
-    erro = False
-    
 
-    if temLetra_exceto(valor, chave):
-        valor = 0
-        erro = True
-        print('foi digitado um valor incorreto')
-    elif ehLetra(valor):
-        if ehNumero(valor[-1]):
-            valor = 0
-            erro = True
-            print('nao consigo entender o numero do final')
-        else:
-            erro = False    
-            # oque = 'nada' 
-            letras = separarLetra(valor)    
-            numeros = separarNumero(valor)  
-            valor = sum(map(int, numeros)) + (len(letras)-len(numeros)) 
-    else:
-        valor = int(valor)
-
-    return chave, valor, erro
-        
-        
-
-
-chave, valor, erro = filtrar('x','103xx')
-print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor))
-
-chave, valor, erro = filtrar('x','103x')
-print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor))
-
-chave, valor, erro = filtrar('x','103x2')
-print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor))
-
-chave, valor, erro = filtrar('x','8')
-print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor))
-
-chave, valor, erro = filtrar('x', '8a')
-print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor))
-
-chave, valor, erro = filtrar('x', '8ax')
-print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor))
+chave, valor, erro, frase = filtrar('x','103xx')
+print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor), '\nfrase:', frase)
+print()
+chave, valor, erro, frase = filtrar('x','103x')
+print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor), '\nfrase:', frase)
+print()
+chave, valor, erro, frase = filtrar('x','8')
+print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor), '\nfrase:', frase)
+print()
+chave, valor, erro, frase = filtrar('x', '8a')
+print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor), '\nfrase:', frase)
+print()
+chave, valor, erro, frase = filtrar('x', '8ax')
+print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor), '\nfrase:', frase)
+print()
+chave, valor, erro, frase = filtrar('x','103x2')
+print('chave:', chave, 'valor:', valor, 'erro:', erro,'\ntipoValor:', type(valor), '\nfrase:', frase)
+print()
--- 06-sistemaLinear3x3/3x3-v9-emDev/uteis.py
+++ 06-sistemaLinear3x3/3x3-v9-emDev/uteis.py
@@ -43,18 +43,47 @@ def separarLetra(letras):
     return list(letras)
 
 
-def temLetra_exceto(chars, exceto='x'):
+def temLetra_exceto(chars, exceto):
     # chars = strings com letras e numero, exceto = a letra que eh para ignorar
     # retorna true pq tem ou false porque nao
     
     chars = list(chars)
-    
     sim = False
     
     for char in chars:
         if ehLetra(char) and char != exceto:
             sim = True
-
-            
     return sim
+
+def filtrar(chave, valor):
+    # filtrar chave e valor de um dicionaria{}
+    
+    erro = False
+    frase = ''
+    
+    # conferindo se hah letra menos "chave"
+    if temLetra_exceto(valor, chave):
+        valor = 0
+        erro = True
+        frase = 'foi digitado um valor incorreto'
+
+    # se tem letra 
+    elif ehLetra(valor):
+        # se tem numero no final de valor de dic{}
+
+        if ehNumero(valor[-1]):
+            
+            frase = f'valor entendido: {valor}{chave}'
+            valor += str(chave)
+            
+        erro = False    
+        letras = separarLetra(valor)    
+        numeros = separarNumero(valor)  
+        valor = sum(map(int, numeros)) + (len(letras)-len(numeros)) 
+
+    else:
+        valor = int(valor)
+
+    return chave, valor, erro, frase
+
 # print(separarNumero('52x78xxx'))

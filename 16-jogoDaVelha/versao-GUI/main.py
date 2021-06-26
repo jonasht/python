547b70da869151b07c48fe08e8778a9561bd8f00
jonasht <jhenriquet@outlook.com.br>
2021-06-26 17:59:24 -0300

    adicionando nova versao em dev

--- 16-jogoDaVelha/versao-GUI/main.py
+++ 16-jogoDaVelha/versao-GUI/main.py
@@ -3,32 +3,59 @@ from tkinter import *
 class Interface(Frame):
     def __init__(self, parent):
         super().__init__(parent)
+        # colocando vez x e O
+        self.vez = ['X', 'O']
         
-        bt00 = Button(self, width=10, height=5)
-        bt01 = Button(self, width=10, height=5)
-        bt02 = Button(self, width=10, height=5)
+        # botoes ...
+        self.bt00 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('00'))
+        self.bt01 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('01'))
+        self.bt02 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('02'))
+        self.bt10 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('10'))
+        self.bt11 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('11'))
+        self.bt12 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('12'))
+        self.bt20 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('20'))
+        self.bt21 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('21'))
+        self.bt22 = Button(self, width=10, height=5, font='arial 15 bold', command=lambda : self.marcar('22'))
+        
+        # colocando botoes com o grid
+        self.bt00.grid(row=0, column=0)
+        self.bt01.grid(row=0, column=1)
+        self.bt02.grid(row=0, column=2)
+        
+        self.bt10.grid(row=1, column=0)
+        self.bt11.grid(row=1, column=1)
+        self.bt12.grid(row=1, column=2)
+        
+        self.bt20.grid(row=2, column=0)
+        self.bt21.grid(row=2, column=1)
+        self.bt22.grid(row=2, column=2)
+        
+        
+    def marcar(self, opcao):
+        if opcao == '00':
+            print(00)
+            self.bt00.config(state=DISABLED, text=self.vez[0])
+        if opcao == '01':
+            self.bt01.config(state=DISABLED, text=self.vez[0])
+        if opcao == '02':
+            self.bt02.config(state=DISABLED, text=self.vez[0])
 
-        bt10 = Button(self, width=10, height=5)
-        bt11 = Button(self, width=10, height=5)
-        bt12 = Button(self, width=10, height=5)
-        
-        bt20 = Button(self, width=10, height=5)
-        bt21 = Button(self, width=10, height=5)
-        bt22 = Button(self, width=10, height=5)
-        
-        # colocando com o grid
-        bt00.grid(row=0, column=0)
-        bt01.grid(row=0, column=1)
-        bt02.grid(row=0, column=2)
-        
-        bt10.grid(row=1, column=0)
-        bt11.grid(row=1, column=1)
-        bt12.grid(row=1, column=2)
-        
-        bt20.grid(row=2, column=0)
-        bt21.grid(row=2, column=1)
-        bt22.grid(row=2, column=2)
-        
+        if opcao == '10':
+            self.bt10.config(state=DISABLED, text=self.vez[0])
+        if opcao == '11':
+            self.bt11.config(state=DISABLED, text=self.vez[0])
+        if opcao == '12':
+            self.bt12.config(state=DISABLED, text=self.vez[0])
+            
+        if opcao == '20':
+            self.bt20.config(state=DISABLED, text=self.vez[0])
+        if opcao == '21':
+            self.bt21.config(state=DISABLED, text=self.vez[0])
+        if opcao == '22':
+            self.bt22.config(state=DISABLED, text=self.vez[0])
+
+        # mudando a vez  
+        self.vez.reverse()
 if __name__ == '__main__':
     root = Tk()
     interface = Interface(root)

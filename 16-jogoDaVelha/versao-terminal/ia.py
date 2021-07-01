f0b1f9455c8ea977a8f5f6b69950b3182ea8511d
jonasht <jhenriquet@outlook.com.br>
2021-07-01 20:46:00 -0300

    fazendo testes

---
+++ 16-jogoDaVelha/versao-terminal/ia.py
@@ -0,0 +1,87 @@
+import colors as c
+from uteis import *
+
+matriz = [[str(i)+str(ii) for ii in range(3)] for i in range(3)]
+
+def find(lista, oque):
+    oque = str(oque)
+    lista = map(str, lista)
+    
+    for i, char in enumerate(lista):
+        if char == oque:
+            return i
+            
+def colocarNumeros(cmatriz):
+    
+    for i, ms in enumerate(cmatriz):
+        for ii, m in enumerate(ms):
+
+            if m == 'X':
+                cmatriz[i][ii] = 3
+            elif m == 'O':
+                cmatriz[i][ii] = 2
+            else:
+                cmatriz[i][ii] = 0
+    return cmatriz
+                
+def somarNumeros(matriz):
+    matriz = colocarNumeros(matriz)
+    numeros = []
+    soma = 0
+
+    soma = 0
+    for i, ms in enumerate(matriz):
+        for ii, m in enumerate(ms):
+            soma += matriz[ii][i]
+        numeros.append(soma)
+            
+        soma = 0
+    
+    soma = 0
+    
+    # /
+    for i in range(3):
+            soma += matriz[2-i][i]
+
+    # soma todas as fileiras
+    numeros.append(soma)
+    for ms in matriz:
+        numeros.append(sum(ms))
+
+    soma=0
+    # \
+    for i in range(3):
+        soma += matriz[i][i]
+    
+    numeros.append(soma)
+    # print(numeros)
+    return numeros
+
+# encontrar espaca na matriz para poder colocar em uma determinada fileira
+def encontrarEspoco(opcao, matriz):
+    fileira = []
+
+    for i, ms in enumerate(matriz):
+        if opcao == 1:
+            if matriz[i][1]== 0:
+                fileira.append([i, 1])
+    # print(fileira)
+    return fileira
+
+
+def fazerJogada(matriz):
+    numeros = somarNumeros(matriz)
+    print(numeros)
+    maior = 0
+    for char in numeros:
+        print(char)
+        # maior 
+        if char > maior and char <= 6:
+            maior = char
+    
+    print(maior)
+    
+    fileira = find(numeros, maior)
+    
+    return encontrarEspoco(fileira, matriz)
+    
--- 16-jogoDaVelha/versao-terminal/teste2.py
+++ 16-jogoDaVelha/versao-terminal/teste2.py
@@ -1,90 +1,90 @@
-from posix import ST_NOATIME
-from uteis import *
+from ia import *
 import colors as c
-
-
+from uteis import *
 
 matriz = [[str(i)+str(ii) for ii in range(3)] for i in range(3)]
 
-def find(lista, oque):
-    oque = str(oque)
-    lista = map(str, lista)
+# def find(lista, oque):
+#     oque = str(oque)
+#     lista = map(str, lista)
     
-    for i, char in enumerate(lista):
-        if char == oque:
-            return i
+#     for i, char in enumerate(lista):
+#         if char == oque:
+#             return i
             
-def colocarNumeros(matriz):
-    for i, ms in enumerate(matriz):
-        for ii, m in enumerate(ms):
+# def colocarNumeros(cmatriz):
+    
+#     for i, ms in enumerate(cmatriz):
+#         for ii, m in enumerate(ms):
 
-            if m == 'X':
-                matriz[i][ii] = 3
-            elif m == 'O':
-                matriz[i][ii] = 2
-            else:
-                matriz[i][ii] = 0
-    return matriz
+#             if m == 'X':
+#                 cmatriz[i][ii] = 3
+#             elif m == 'O':
+#                 cmatriz[i][ii] = 2
+#             else:
+#                 cmatriz[i][ii] = 0
+#     return cmatriz
                 
-def somarNumeros(matriz):
-    matriz = colocarNumeros(matriz)
-    numeros = []
-    soma = 0
+# def somarNumeros(matriz):
+#     matriz = colocarNumeros(matriz)
+#     numeros = []
+#     soma = 0
 
-    soma = 0
-    for i, ms in enumerate(matriz):
-        for ii, m in enumerate(ms):
-            soma += matriz[ii][i]
-        numeros.append(soma)
+#     soma = 0
+#     for i, ms in enumerate(matriz):
+#         for ii, m in enumerate(ms):
+#             soma += matriz[ii][i]
+#         numeros.append(soma)
             
-        soma = 0
+#         soma = 0
     
-    soma = 0
+#     soma = 0
     
-    # /
-    for i in range(3):
-            soma += matriz[2-i][i]
+#     # /
+#     for i in range(3):
+#             soma += matriz[2-i][i]
 
-    # soma todas as fileiras
-    numeros.append(soma)
-    for ms in matriz:
-        numeros.append(sum(ms))
+#     # soma todas as fileiras
+#     numeros.append(soma)
+#     for ms in matriz:
+#         numeros.append(sum(ms))
 
-    soma=0
-    # \
-    for i in range(3):
-        soma += matriz[i][i]
+#     soma=0
+#     # \
+#     for i in range(3):
+#         soma += matriz[i][i]
     
-    numeros.append(soma)
-    # print(numeros)
-    return numeros
+#     numeros.append(soma)
+#     # print(numeros)
+#     return numeros
 
-def fazerJogada(matriz):
-    numeros = somarNumeros(matriz)
-    print(numeros)
-    maior = 0
-    for char in numeros:
-        print(char)
-        # maior 
-        if char > maior and char <= 6:
-            maior = char
+# # encontrar espaca na matriz para poder colocar em uma determinada fileira
+# def encontrarEspoco(opcao, matriz):
+#     fileira = []
+
+#     for i, ms in enumerate(matriz):
+#         if opcao == 1:
+#             if matriz[i][1]== 0:
+#                 fileira.append([i, 1])
+#     # print(fileira)
+#     return fileira
+
+
+# def fazerJogada(matriz):
+#     numeros = somarNumeros(matriz)
+#     print(numeros)
+#     maior = 0
+#     for char in numeros:
+#         print(char)
+#         # maior 
+#         if char > maior and char <= 6:
+#             maior = char
     
-    print(maior)
-    
-    fileira = find(numeros, maior)
-    mostrar(matriz)
-    print(encontrarEspoco(fileira, matriz)
-          
-# encontrar espaca na matriz para poder colocar em uma determinada fileira
-def encontrarEspoco(opcao, matriz):
-    fileira = []
-
-    for i, ms in enumerate(matriz):
-        if opcao == 1:
-            if matriz[i][1]== 0:
-                fileira.append(i, 1)
-    return fileira
-
+    # print(maior)
+    # 
+    # fileira = find(numeros, maior)
+    # 
+    # return encontrarEspoco(fileira, matriz)
     
 mostrar(matriz)
 colocar('O', 0, 0, matriz)
@@ -96,9 +96,11 @@ colocar('O', 2, 0, matriz)
 
 
 mostrar(matriz)
-# somarNumeros(matriz)
-print(fazerJogada(matriz))
+# mandarmatriz = matriz[:]
+jogada = fazerJogada(matriz)
 
+colocar('X', jogada[0][0], jogada[0][1], matriz)
+mostrar(matriz)
 
 
         

6f76aa5a75b7c76cfa22b60d4527d7dbe4472482
jonasht <jhenriquet@outlook.com.br>
2021-08-05 11:23:34 -0300

    adicionando novos estudos

--- estudos-E-paraVerComoFunciona/alura/python3introorientacaoobjetos/conta.py
+++ estudos-E-paraVerComoFunciona/alura/python3introorientacaoobjetos/conta.py
@@ -3,18 +3,27 @@ class Conta:
         
         print('funcionando')
         self.__numero = numero
-        self.__titulo = titular
+        self.__titular = titular
         self.__saldo = saldo
         self.__limete = limite
         
         
-    def extrato(self, conta):
+    def extrato(self):
+        print('=-'*20+'=')
+        print(f'nome: {self.__titular}')
         print(f'saldo: {self.__saldo}')
-
+        print('=-'*20+'=')
     def depositar(self, valor):
         self.__saldo += valor
     
     def secar(self, valor):
         self.__saldo -= valor
+    
+    def transferir(self, origem, destino, valor):
+        origem.sacar(valor)
+        destino.depositar(valor)
+
+
+
         
 #conta = Conta(123, 'jonas', 520.0, 1000.0)

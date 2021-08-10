8f3140c5bd63b45d98f4b509617f97b913a61bdc
jonasht <jhenriquet@outlook.com.br>
2021-08-09 22:18:17 -0300

    fazendo testes

--- 11-auroTUI/0-auroGame/teste/main.py
+++ 11-auroTUI/0-auroGame/teste/main.py
@@ -27,72 +27,81 @@ fgfim = Fore.RESET
 fim = Style.NORMAL
 
 
-
-a = auroGame(largura=100, altura=10, background=' ')
-nomesObjetos = list(range(30))
-shuffle(nomesObjetos)
-
-
-for i, nome in enumerate(nomesObjetos):
-    if len(str(nome)) == 1:
-        a.set_objeto((' ' + str(nome)), x=i+i+i, y=-5, inicio=bgblue+Fore.WHITE, fim=fim)
-    else:   
-        a.set_objeto(nome, x=i+i+i, y=-5, inicio=bgblue, fim=fim)
-    a.mostrar()
-
-
-
-guardarNumero = 0
-def paraFrente(nome2, nome1, tempo=0.1):
-    nome2 = str(nome2)
-    nome1 = str(nome1)
-    
-
-    if len(nome2) == 1:
-        nome2 = ' ' + nome2
-    if len(nome1) == 1:
-        nome1 = ' ' + nome1
-    a.reset_objeto(nome=nome1, inicio=bggreen+fgblack)
-    a.reset_objeto(nome=nome2, inicio=bggreen+fgblack)
-    a.mostrar()
-    sleep(tempo)
-
-    for _ in range(3):
-        a.move(nome2, op='^')
-        a.move(nome=nome1, op='v')
-        a.mostrar()
-        sleep(tempo)
-
-    for _ in range(3):
-        a.move(nome=nome2, op='<')
-        a.move(nome=nome1, op='>')
-        a.mostrar()
-        sleep(tempo)
+class Principal:
+    def __init__(self, tempo=.1):
+        self.__tempo = tempo
         
-    for _ in range(3):
-        a.move(nome=nome2, op='v')
-        a.move(nome=nome1, op='^')
-        a.mostrar()
-        sleep(tempo)
+        self.a = auroGame(largura=200, altura=10, background=' ')
+        self.nomesObjetos = list(range(60))
+        shuffle(self.nomesObjetos)
 
-    a.reset_objeto(nome=nome1, inicio=bgblue+fgwhite)
-    a.reset_objeto(nome=nome2, inicio=bgblue+fgwhite)
-    a.mostrar()
-    sleep(tempo)
+
+        for i, nome in enumerate(self.nomesObjetos):
+            if len(str(nome)) == 1:
+                self.a.set_objeto((' ' + str(nome)), x=i+i+i, y=-5, inicio=bgblue+Fore.WHITE, fim=fim)
+            else:   
+                self.a.set_objeto(nome, x=i+i+i, y=-5, inicio=bgblue+Fore.WHITE, fim=fim)
+            self.a.mostrar()
+
+
+
+        self.guardarNumero = 0
     
-def Maneira():
-    for c in np.arange(len(nomesObjetos)):
-        for i, nomeNum in enumerate(nomesObjetos):
+        self.a.mostrar()
+        self.Maneira()
+        
+    def paraFrente(self, nome2, nome1):
+        nome2 = str(nome2)
+        nome1 = str(nome1)
+        
 
-            if i+1 == len(nomesObjetos):
-                continue
-            else:
-                if nomesObjetos[i] > nomesObjetos[i+1]:
-                    guardarNumero = nomesObjetos[i]
-                    nomesObjetos[i] = nomesObjetos[i+1]
-                    nomesObjetos[i+1] = guardarNumero
-                    paraFrente(nomesObjetos[i], nomesObjetos[i+1])
+        if len(nome2) == 1:
+            nome2 = ' ' + nome2
+        if len(nome1) == 1:
+            nome1 = ' ' + nome1
+        self.a.reset_objeto(nome=nome1, inicio=bggreen+fgblack)
+        self.a.reset_objeto(nome=nome2, inicio=bggreen+fgblack)
+        self.a.mostrar()
+        sleep(self.__tempo)
 
-a.mostrar()
-Maneira()
-# paraFrente(' 1', ' 0', 2)
+        for _ in range(3):
+            self.a.move(nome2, op='^')
+            self.a.move(nome=nome1, op='v')
+            self.a.mostrar()
+            sleep(self.__tempo)
+
+        for _ in range(3):
+            self.a.move(nome=nome2, op='<')
+            self.a.move(nome=nome1, op='>')
+            self.a.mostrar()
+            sleep(self.__tempo)
+            
+        for _ in range(3):
+            self.a.move(nome=nome2, op='v')
+            self.a.move(nome=nome1, op='^')
+            self.a.mostrar()
+            sleep(self.__tempo)
+
+        self.a.reset_objeto(nome=nome1, inicio=bgblue+fgwhite)
+        self.a.reset_objeto(nome=nome2, inicio=bgblue+fgwhite)
+        self.a.mostrar()
+        sleep(self.__tempo)
+    
+    
+    def Maneira(self):
+        for c in np.arange(len(self.nomesObjetos)):
+            for i, nomeNum in enumerate(self.nomesObjetos):
+
+                if i+1 == len(self.nomesObjetos):
+                    continue
+                else:
+                    if self.nomesObjetos[i] > self.nomesObjetos[i+1]:
+                        self.guardarNumero = self.nomesObjetos[i]
+                        self.nomesObjetos[i] = self.nomesObjetos[i+1]
+                        self.nomesObjetos[i+1] = self.guardarNumero
+                        self.paraFrente(self.nomesObjetos[i], self.nomesObjetos[i+1])
+
+
+if __name__ == '__main__':
+
+    root = Principal(tempo=0)

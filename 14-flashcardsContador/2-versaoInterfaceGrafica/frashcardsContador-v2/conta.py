99ee38ad8ac5929baccd0296e8900b254fa8f422
jonasht <jhenriquet@outlook.com.br>
2021-08-21 21:27:09 -0300

    nova versao

---
+++ 14-flashcardsContador/2-versaoInterfaceGrafica/frashcardsContador-v2/conta.py
@@ -0,0 +1,29 @@
+
+class Conta:
+    def __init__(self, qtdCartas=30):
+        self.qtdCartas = qtdCartas
+        self.recomecar = list()
+        self.comecar = list()
+        
+    def somarRecomecar(self, numero):
+        self.recomecar.append(numero)
+        
+    def somarComecar(self, numero):
+        self.comecar.append(numero)
+        
+    def get_recomecar(self):
+        return self.recomecar
+    def get_totalRecomecar(self):
+        return sum(self.recomecar)
+
+    def get_comecar(self):
+        return self.comecar
+    
+    def get_totalComecar(self):
+        return sum(self.comecar)
+
+
+    def get_total(self):
+        return sum(self.recomecar) + sum(self.comecar)
+    def get_restanteDeCartas(self):
+        return self.qtdCartas - self.get_total() 
---
+++ 14-flashcardsContador/2-versaoInterfaceGrafica/frashcardsContador-v2/frame.py
@@ -0,0 +1,69 @@
+from tkinter import * 
+
+
+class Interface(Frame):
+    def __init__ (self, parent):
+        super().__init__(parent)
+        self.frameRecomecar = Frame(self)
+        self.frameComecar = Frame(self)
+        self.frameTotal = Frame(self)
+        
+
+        self.lb1 = Label(self.frameRecomecar, text='Cartas para Recomeçar:', width=20, font='arial 16')
+        self.lb_numero1 = Label(self.frameRecomecar, text='0', fg='purple', font='arial 16 bold', width=2)
+
+        self.lb2 = Label(self.frameComecar,   text='Cartas para   Começar:', width=20, font='arial 16')
+        self.lb_numero2 = Label(self.frameComecar, text='0', fg='blue', font='arial 16 bold', width=2)
+
+        self.lb_falta = Label(self.frameTotal, text='Faltam:', width=8, font='arial 16')
+        self.lb_faltaNum = Label(self.frameTotal, text='0', fg='red', width=2, font='arial 16 bold')
+
+        self.lb_total = Label(self.frameTotal, text='     Total:', width=9, font='arial 16')
+        self.lb_totalNum = Label(self.frameTotal, text='0', fg='blue', font='arial 16 bold')
+
+        self.lb_lista1 = Label(self, text='', fg='purple')
+        self.lb_lista2 = Label(self, text='', fg='blue')
+
+        self.lb_falta.pack(side=LEFT)
+
+        self.lb_faltaNum.pack(side=LEFT)
+        self.lb_total.pack(side=LEFT)
+        self.lb_totalNum.pack(side=RIGHT)
+
+ 
+        
+        self.lb1.pack(side=LEFT)
+        self.lb_numero1.pack(side=RIGHT)
+
+        self.lb2.pack(side=LEFT)
+        self.lb_numero2.pack(side=RIGHT)
+
+        self.frameRecomecar.pack()
+        self.frameComecar.pack()
+        self.frameTotal.pack()
+        
+        self.lb_lista1.pack()
+        self.lb_lista2.pack()
+        
+    def atualizarTotal(self, n):
+        self.lb_totalNum.config(text=n)
+    
+    def atualizarRecomecar(self, n):
+        self.lb_numero1.config(text=n)  
+        
+    def atualizarComecar(self, n):
+        self.lb_numero2.config(text=n)
+    
+    def atualizarRestanteDeCartas(self, n):
+        if n > 0:
+            self.lb_faltaNum.config(text=n, fg='blue')         
+        else:
+            self.lb_faltaNum.config(text=n, fg='red')
+            
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry('400x300')
+
+    frame = Interface(root)
+    frame.pack()
+    root.mainloop()

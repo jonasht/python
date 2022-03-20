6e06e2d03ad1c91a0a2a5dc770bda08c3cce24e4
jonasht <jhenriquet@outlook.com.br>
2022-03-19 22:15:50 -0300

    make tests

--- 13-criptografiaSimples/versaoInterfaceGrafica/teste/with.py
+++ 13-criptografiaSimples/versaoInterfaceGrafica/teste/with.py
@@ -6,37 +6,52 @@ def read_dir() -> list:
     txts = [arq for arq in arquivos if arq.lower().endswith('.txt')]
 
     return txts
+    
 
 
 
 var = 'algo algo algo algo'
 
 
-def write(var):
-    dirs = read_dir()
-    contador = 0
-    
-    while True:
-        if str(contador) is not dirs[contador]:
-            txt_endereco = './txtTeste/' + 'txt' + str(contador) + '.txt'
-            print('entrou', txt_endereco)
-            with open(txt_endereco, 'w') as arq:
-                arq.write(var)
+def write(nameArq, txt):
+    if not os.path.exists('txtTeste'):
+        os.makedirs('txtTeste')
 
-            break
-        print('fora')
+    txt_endereco = './txtTeste/' + nameArq + '.txt'
+    with open(txt_endereco, 'w') as arq:
+        arq.write(txt) 
+
         
-        contador += 1
 
-# write(var)
-dirs = read_dir()
 
-for i in range(10):
-    for dir in dirs:
-        if str(i) in dir:
-            print(i, 'true')
-            break
-        else:
-            print(False)
+def dir_in(dir):
+    if not os.path.exists('txtTeste'):
+        os.makedirs('txtTeste')
+    dir = './txtTeste/'+ dir +'.txt'
+    # dir = 'txt'
+    
+    if dir in read_dir():
+        return True
+    else:
+        return False
 
-# read_dir()
+def choose_nameDir():
+    i = 0
+    while True:
+        nome = 'txt'+str(i)
+        if not dir_in(nome):    
+            return nome
+        i += 1
+if __name__ == '__main__':
+    import random 
+    def random_num():
+        nums = ''
+        for _ in range(100):
+            nums += str(random.randint(10, 1000000))
+        return nums
+    for i in range(100):
+        
+        nextname = choose_nameDir()
+        write(nextname, random_num())
+
+    

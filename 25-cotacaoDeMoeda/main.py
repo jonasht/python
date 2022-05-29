2e94d7debd29ce0bbb0e9f58fe0ff14258602093
jonasht <jhenriquet@outlook.com.br>
2022-05-29 16:48:57 -0300

    update, more beautiful

--- 25-cotacaoDeMoeda/main.py
+++ 25-cotacaoDeMoeda/main.py
@@ -1,35 +1,37 @@
-from turtle import color
 import requests
-from tkinter import Tk, ttk
+from tkinter import EW, Tk, ttk
 class Fr(ttk.Frame):
     def __init__(self, parent) -> None:
         super().__init__(parent)
         self.fr_up = ttk.Frame()
-        self.texto = ttk.Label(self.fr_up, text="Clique no botão para ver as cotações de moedas")
-        self.botao = ttk.Button(self.fr_up, text="Buscar cotações", command=self.pegar_cotacoes)
-        
+        self.texto = ttk.Label(self.fr_up, text="Clique no botão para atualizar as cotações de moedas")
+        self.botao = ttk.Button(self.fr_up, text="Atualizar", command=self.pegar_cotacoes)
+        # self.botao.config(width=20)
         self.texto.grid(row=0)
-        self.botao.grid(row=1)
+        self.botao.grid(row=1, sticky=EW)
 
 
         self.fr_ct = ttk.Frame()
-        self.lb_dolar1 = ttk.Label(self.fr_ct, text='Dolar:')
-        self.lb_dolar2 = ttk.Label(self.fr_ct, text='')
-        self.lb_euro1 = ttk.Label(self.fr_ct, text='Euro:')
-        self.lb_euro2 = ttk.Label(self.fr_ct, text='')
-        self.lb_BTC1 = ttk.Label(self.fr_ct, text='BTC:')
-        self.lb_BTC2 = ttk.Label(self.fr_ct, text='')
+        self.lbfr_dolar = ttk.LabelFrame(self.fr_ct, text='Dolar')
+        self.lbfr_euro = ttk.LabelFrame(self.fr_ct, text='Euro')
+        self.lbfr_BTC = ttk.LabelFrame(self.fr_ct, text='BTC')
 
-        self.lb_dolar1.grid(row=0, column=0)
-        self.lb_dolar2.grid(row=0, column=1)
-        self.lb_euro1.grid(row=1, column=0)
-        self.lb_euro2.grid(row=1, column=1)
-        self.lb_BTC1.grid(row=2, column=0)
-        self.lb_BTC2.grid(row=2, column=1)
+        self.lb_dolar2 = ttk.Label(self.lbfr_dolar, text='')
+        self.lb_euro2 = ttk.Label(self.lbfr_euro, text='')
+        self.lb_BTC2 = ttk.Label(self.lbfr_BTC, text='')
+
+        self.lb_dolar2.grid(row=0, column=0)
+        self.lb_euro2.grid(row=1, column=0)
+        self.lb_BTC2.grid(row=2, column=0)
+
+        self.lbfr_dolar.grid(row=0, column=0, ipadx=20, sticky=EW)
+        self.lbfr_euro.grid(row=1, column=0, ipadx=20, sticky=EW)
+        self.lbfr_BTC.grid(row=2, column=0, ipadx=20, sticky=EW)
         
         self.fr_up.grid()
         self.fr_ct.grid(row=2)
 
+        self.pegar_cotacoes()
     def pegar_cotacoes(self):
         requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 
@@ -38,7 +40,9 @@ class Fr(ttk.Frame):
         cotacao_dolar = requisicao_dic['USDBRL']['bid']
         cotacao_euro = requisicao_dic['EURBRL']['bid']
         cotacao_btc = requisicao_dic['BTCBRL']['bid']
-
+        cotacao_dolar = 'R$ '+ str(cotacao_dolar.replace('.', ','))
+        cotacao_euro = 'R$ '+ str(cotacao_euro.replace('.', ','))
+        cotacao_btc = 'R$ '+ str(cotacao_btc)
         self.lb_dolar2.config(text=cotacao_dolar)
         self.lb_euro2.config(text=cotacao_euro)
         self.lb_BTC2.config(text=cotacao_btc)

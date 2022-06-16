035257bdbfb1666bfbd95ebc344756088aa00d8b
jonasht <jhenriquet@outlook.com.br>
2022-06-15 22:21:57 -0300

    adding new code

--- programaSimplex_emDev/fr1.py
+++ programaSimplex_emDev/fr1.py
@@ -1,14 +1,24 @@
-from tkinter import Tk, ttk
+from tkinter import END, Tk, ttk
+import lippy 
 
 
 
 class Fr1(ttk.Frame):
     def __init__(self, parent, con=None):
         super().__init__(parent)
-        self.lb_titulo = ttk.Label(self, text='funcionando')
+        self.lb_titulo = ttk.Label(self, text='')
         self.lb_titulo.grid()
-
+        
+        self.fr_cima = ttk.Frame(self)
         self.fr_meio = ttk.Frame(self)
+        # linha z =-=-=-=-==-=-=-=-
+        self.etd1x = ttk.Entry(self.fr_cima)
+        self.lb1x = ttk.Label(self.fr_cima, text='x1')
+        self.etd2x = ttk.Entry(self.fr_cima)
+        self.lb2x = ttk.Label(self.fr_cima, text='x2')
+        self.etd3x = ttk.Entry(self.fr_cima)
+        self.lb3x = ttk.Label(self.fr_cima, text='x3')
+
         # linha 1 =-=-=-=-==-=-=-=-
         self.etdx11 = ttk.Entry(self.fr_meio)
         self.etdx12 = ttk.Entry(self.fr_meio)
@@ -39,6 +49,19 @@ class Fr1(ttk.Frame):
         self.lbx33 = ttk.Label(self.fr_meio, text='X3')
         self.lbEg3 = ttk.Label(self.fr_meio, text='=')
         self.etdEg3 = ttk.Entry(self.fr_meio)
+        # botao
+        self.bt_solve = ttk.Button(self, text='calcular', command=self.to_solve)
+        # label solve =-=-=-=-=-=-=
+        self.lb_solve = ttk.Label(self, text='')
+
+         # colocando linha z =-=-=-=-==-=-=-=-
+        self.etd1x.grid(row=0, column=0)
+        self.lb1x.grid(row=0, column=1)
+        self.etd2x.grid(row=0, column=2)
+        self.lb2x.grid(row=0, column=3)
+        self.etd3x.grid(row=0, column=4)
+        self.lb3x.grid(row=0, column=5) 
+        
          # colocando linha 1 =-=-=-=-==-=-=-=-
         self.etdx11.grid(row=1,column=1)
         self.lbx11.grid(row=1,column=2) 
@@ -68,10 +91,77 @@ class Fr1(ttk.Frame):
         self.lbx33.grid(row=3,column=6) 
         self.lbEg3.grid(row=3, column=7)
         self.etdEg3.grid(row=3, column=8)
-
+        
+        self.fr_cima.grid()
         self.fr_meio.grid()
+        # colocando botao solve
+        self.bt_solve.grid()
+        self.lb_solve.config(text='resultado')
+        self.lb_solve.grid()
+        self.put_default()
+    def put_default(self):
+        self.etd1x.insert(END, 6)
+        self.etd2x.insert(END, 6)
+        self.etd3x.insert(END, 6)
+
+        # Matriz
+        self.etdx11.insert(END,4 )
+        self.etdx12.insert(END, 1)
+        self.etdx13.insert(END,1 )
+        self.etdEg1.insert(END, 5)
+
+        self.etdx21.insert(END, 1)
+        self.etdx22.insert(END, 2)
+        self.etdx23.insert(END, 0)
+        self.etdEg2.insert(END, 3)
+
+        self.etdx31.insert(END, 0)
+        self.etdx32.insert(END, 0.5)
+        self.etdx33.insert(END, 4)
+        self.etdEg3.insert(END, 8)
+        
+    def to_solve(self):
+        z1 = self.etd1x.get() 
+        z2 = self.etd2x.get() 
+        z3 = self.etd3x.get() 
+
+        print(z1, z2, z3)
+        
+        x11  = self.etdx11.get()
+        x12  = self.etdx12.get()
+        x13  = self.etdx13.get()
+        Eg1  = self.etdEg1.get()
+        
+        x21  = self.etdx21.get()
+        x22  = self.etdx22.get()
+        x23  = self.etdx23.get()
+        Eg2  = self.etdEg2.get()
+
+        x31  = self.etdx31.get()
+        x32  = self.etdx32.get()
+        x33  = self.etdx33.get()
+        Eg3  = self.etdEg3.get()
+        
+        c = [z1, z2, z3]
+        a = [
+            [x11, x12, x13],
+            [x21, x22, x23],
+            [x31, x32, x33]
+        ]
+        b = [Eg1, Eg2, Eg3]
+
+        simplex = lippy.SimplexMethod(c, a, b)
+        solution, value = simplex.solve()
+        
+        solve = f'{solution}\n{value}'
+        self.lb_solve.config(text=solve)
+
+
+
+        
 if __name__ == '__main__':
     root = Tk()
+    root.title('simplex')
     fr = Fr1(root)
     fr.pack()
     root.geometry('700x500')

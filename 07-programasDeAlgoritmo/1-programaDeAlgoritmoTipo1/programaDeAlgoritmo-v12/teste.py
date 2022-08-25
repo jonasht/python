307b27965b0af742548a83a5d8bf564289e84ad9
jonasht <jhenriquet@outlook.com.br>
2022-08-25 10:03:59 -0300

    let it faster, add version12

---
+++ 07-programasDeAlgoritmo/1-programaDeAlgoritmoTipo1/programaDeAlgoritmo-v12/teste.py
@@ -0,0 +1,24 @@
+# versão 10, versao o que ha de novo:
+# a cor amarela para retroceder
+# quando terminar a ultima cor some 
+from algoritmo2 import *
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# definir de tamanho, o tempo e o pixel de character
+# M2(tamanho, tempo, tamanho do pixel ou oque vai estar nele)
+
+pr = M2(90, 0, "   ")
+# pr = M2(30, .05, "   ")
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+# se aparecer tudo errado, tem que diminuir a fonte do terminal (ctrl + -)
+# também se aparecer tudo errado mostrar [alguma coisa, o terminal não suporta ansii
+# o codigo foi feito no gnu/linux, caso for no windows deve-se importar colorama
+
+# import colorama
+# colorama.init()
+
+# para instalar colorama no windows
+# pip install colorama
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+pr.Maneira2()   

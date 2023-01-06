2a56210a6730a8662c88d3a8913a3dbed95c2c85
jonasht <jhenriquet@outlook.com.br>
2023-01-06 00:37:36 -0300

    update interface with ttk bootstrap

--- 05-cotacaoDeMoeda/main.py
+++ 05-cotacaoDeMoeda/main.py
@@ -1,5 +1,6 @@
 import requests
-from tkinter import EW, Tk, ttk
+from tkinter import BOTTOM, EW, Tk
+import ttkbootstrap as ttk
 from PIL import ImageTk, Image
 
 
@@ -7,43 +8,43 @@ class Fr(ttk.Frame):
     def __init__(self, parent) -> None:
         super().__init__(parent)
         
-        self.fr_up = ttk.Frame()
-        self.texto = ttk.Label(self.fr_up, text='Clique no botão para atualizar as cotações de moedas')
-        self.botao = ttk.Button(self.fr_up, text='Atualizar', command=self.get_cotacao)
-        self.texto.grid(row=0)
-        self.botao.grid(row=1, sticky=EW)
+        self.fr_up = ttk.Frame(self)
+        self.texto = ttk.Label(self.fr_up, text='Clique no botão para atualizar as cotações de moedas', bootstyle='info')
+        self.botao = ttk.Button(self.fr_up, text='Atualizar', command=self.get_cotacao, bootstyle='success')
+        self.texto.grid(row=0, pady=6)
+        self.botao.grid(row=1, sticky=EW, pady=6)
 
 
-        self.fr_ct = ttk.Frame()
-        self.lbfr_dolar = ttk.LabelFrame(self.fr_ct, text='Dolar')
-        self.lbfr_euro = ttk.LabelFrame(self.fr_ct, text='Euro')
-        self.lbfr_BTC = ttk.LabelFrame(self.fr_ct, text='Bitcoin')
+        self.fr_ct = ttk.Frame(self)
+        self.lbfr_dolar = ttk.LabelFrame(self.fr_ct, text='Dolar', bootstyle='info')
+        self.lbfr_euro = ttk.LabelFrame(self.fr_ct, text='Euro', bootstyle='info')
+        self.lbfr_BTC = ttk.LabelFrame(self.fr_ct, text='Bitcoin', bootstyle='info')
         
         # img =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
         self.img_dolar = ImageTk.PhotoImage(Image.open('./img/dolar.png'))
         self.lbimg_dolar = ttk.Label(self.lbfr_dolar, image=self.img_dolar)
-        self.lbimg_dolar.grid(row=0, column=0)
+        self.lbimg_dolar.grid(row=0, column=0, padx=30)
         
         self.img_euro = ImageTk.PhotoImage(Image.open('./img/euro.png'))
         self.lbimg_euro = ttk.Label(self.lbfr_euro, image=self.img_euro)
-        self.lbimg_euro.grid(row=0, column=0)
+        self.lbimg_euro.grid(row=0, column=0, padx=30)
         
         self.img_BTC = ImageTk.PhotoImage(Image.open('./img/bitcoin.png'))
         self.lbimg_BTC = ttk.Label(self.lbfr_BTC, image=self.img_BTC)
-        self.lbimg_BTC.grid(row=0, column=0)
+        self.lbimg_BTC.grid(row=0, column=0, padx=30)
         # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=        
 
-        self.lb_dolar = ttk.Label(self.lbfr_dolar, text='')
-        self.lb_euro2 = ttk.Label(self.lbfr_euro, text='')
-        self.lb_BTC2 = ttk.Label(self.lbfr_BTC, text='')
+        self.lb_dolar = ttk.Label(self.lbfr_dolar, text='', font='Arial 16 bold')
+        self.lb_euro2 = ttk.Label(self.lbfr_euro, text='', font='Arial 16 bold')
+        self.lb_BTC2 = ttk.Label(self.lbfr_BTC, text='', font='Arial 16 bold')
 
-        self.lb_dolar.grid(row=0, column=1)
-        self.lb_euro2.grid(row=0, column=1)
-        self.lb_BTC2.grid(row=0, column=1)
+        self.lb_dolar.grid(row=0, column=1, pady=6, padx=10)
+        self.lb_euro2.grid(row=0, column=1, pady=6, padx=10)
+        self.lb_BTC2.grid(row=0, column=1, pady=6, padx=10)
 
-        self.lbfr_dolar.grid(row=0, column=0, ipadx=30, sticky=EW)
-        self.lbfr_euro.grid(row=1, column=0, ipadx=20, sticky=EW)
-        self.lbfr_BTC.grid(row=2, column=0, ipadx=20, sticky=EW)
+        self.lbfr_dolar.grid(row=0, column=0, ipadx=30, sticky=EW, pady=6)
+        self.lbfr_euro.grid(row=1, column=0, ipadx=20, sticky=EW, pady=6)
+        self.lbfr_BTC.grid(row=2, column=0, ipadx=20, sticky=EW, pady=6)
         
         self.fr_up.grid()
         self.fr_ct.grid(pady=10)
@@ -65,14 +66,23 @@ class Fr(ttk.Frame):
         self.lb_BTC2.config(text=cotacao_btc)
 def main():
     
-    janela = Tk()
-    fr = Fr(janela)
-    janela.resizable(width=False, height=False)
-    fr.grid()
-    janela.title('Cotação de Moedas')
+    window = Tk()
+    fr = Fr(window)
+    window.resizable(width=False, height=False)
+    fr.pack()
+    lb_exit = ttk.Label(window, text='esc/q para sair\nEsc/q to exit', bootstyle='warning')
+    lb_exit.pack(side=BOTTOM)
+
+    window.bind('q', lambda x: window.destroy())
+    window.bind('<Escape>', lambda x: window.destroy())
+    
+    # grid()
+    window.geometry('600x500')
+    window.title('Cotação de Moedas')
 
 
-    janela.mainloop()
+    window.mainloop()
+    
 if __name__ == '__main__':
     main()
     

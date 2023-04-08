1e98f77d9d8842f2e804ac3b1e78db81893fcfab
jonasht <jhenriquet@outlook.com.br>
2023-04-08 17:57:58 -0300

    version 3.1

--- 28-downloadVideoYT/utils.py
+++ 28-downloadVideoYT/utils.py
@@ -12,7 +12,12 @@ def write(path, text):
 
 
 def get_configTheme():
-    return read('./config.txt')
+    try:
+        return read('./config.txt')
+    except:
+        theme = 'cyborg'
+        set_configTheme(theme)
+        return theme
 
 def set_configTheme(theme):
     write('./config.txt', theme)
@@ -22,15 +27,15 @@ if __name__ == "__main__":
     # write('text.txt', 'hello world')
 
     # print(read('./text.txt'))
-    set_configTheme('cyborg')
+    # set_configTheme('cyborg')
     print(get_configTheme())
     
     import ttkbootstrap as ttk
     
-    w = ttk.Window()
-    themes = w.style.theme_names()
-    print(themes)
+    # w = ttk.Window()
+    # themes = w.style.theme_names()
+    # print(themes)
     
-    print(themes.index('lumen'))
-    w.style.theme_use(get_configTheme())
-    print(w.style.theme_use())
+    # print(themes.index('lumen'))
+    # w.style.theme_use(get_configTheme())
+    # print(w.style.theme_use())

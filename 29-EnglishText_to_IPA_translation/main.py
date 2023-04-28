e22e97107255e9e46b414e83f3c344471dec2afe
jonasht <jhenriquet@outlook.com.br>
2023-04-27 21:33:23 -0300

    version 3, change grid to pack

--- 29-EnglishText_to_IPA_translation/main.py
+++ 29-EnglishText_to_IPA_translation/main.py
@@ -9,52 +9,55 @@ import eng_to_ipa as ipa
 class Frame(ttk.Frame):
     def __init__(self, parent) -> None:
         super().__init__(parent)
+        
+        # frames lines
+        self.line1 = ttk.Frame(self)
+        self.line2 = ttk.Frame(self)
+        self.line3 = ttk.Frame(self)
+        self.line4 = ttk.Frame(self)
+        
 
-        self.bt_translate = ttk.Button(self, text='Translate', padding=20)
+        self.lb1 = ttk.Label(self.line1, text='english text:')
+        self.lb2 = ttk.Label(self.line1, text=':alphabet phonetic international')
+        self.tt1 = ttk.Text(self.line2)
+        self.tt2 = ttk.Text(self.line2)
+        
+        self.bt_translate = ttk.Button(self.line3, text='Translate', padding=20)
         self.bt_translate.configure(command=self.cmd_translate)
-        self.lb1 = ttk.Label(self, text='english text:')
-        self.lb2 = ttk.Label(self, text='alphabet phonetic international:')
         self.lb1.config(bootstyle=INFO)
-        self.lb2.config(bootstyle=INFO)
-        width_tt = 130
-        height_tt = 50
+        self.lb2.config(bootstyle=INFO)        
 
-        self.tt1 = ttk.Text(self, width=width_tt, height=height_tt)
-        self.tt2 = ttk.Text(self, width=width_tt, height=height_tt)
         
-        self.fr_file = ttk.Frame(self)
-        self.lb_file = ttk.Label(self.fr_file, text='file:')
-        self.et_file = ttk.Entry(self.fr_file)
-        self.bt_file = ttk.Button(self.fr_file, text='Open file', command=self.cmd_open)
+        self.lb_file = ttk.Label(self.line4, text='file:')
+        self.et_file = ttk.Entry(self.line4)
+        self.bt_file = ttk.Button(self.line4, text='Open file', command=self.cmd_open)
         
-        self.lb1.grid(row=1, column=0, sticky=EW, padx=7)
-        self.lb2.grid(row=1, column=1, sticky=EW)
-        self.tt1.grid(row=2, column=0)
-        self.tt2.grid(row=2, column=1)
-        self.bt_translate.grid(row=3, column=0, columnspan=2, pady=10, padx=5, sticky=EW)
+        self.lb_save = ttk.Label(self.line4, text='save:')
+        self.et_save = ttk.Entry(self.line4)
+        self.bt_save = ttk.Button(self.line4, text='save file', command=self.save_file)
 
-        self.lb_file.grid(row=0, column=0, padx=5,)
-        self.et_file.grid(row=0, column=1, padx=5,)
-        self.bt_file.grid(row=0, column=3, padx=5,)
-
-        self.et_file.config(width=90)
-        self.bt_file.config(width=30)
-        self.fr_file.grid(row=4, column=0, sticky=EW, padx=5,)
         
+        self.lb1.pack(side=LEFT, fill=X)
+        self.lb2.pack(side=RIGHT,fill=X)
+        self.tt1.pack(side=LEFT, anchor=W, expand=True, fill=BOTH)
+        self.tt2.pack(side=LEFT, anchor=E, expand=True, fill=BOTH)
+        self.bt_translate.pack(fill=X, expand=True, pady=5)
         
-        self.fr_save = ttk.Frame(self)
-        self.lb_save = ttk.Label(self.fr_save, text='save:')
-        self.et_save = ttk.Entry(self.fr_save)
-        self.bt_save = ttk.Button(self.fr_save, text='save file', command=self.save_file)
+        self.lb_file.pack(side=LEFT)
+        self.et_file.pack(side=LEFT, expand=True, fill=X)
+        self.bt_file.pack(side=LEFT, expand=True, fill=X, padx=5)
         
+        self.bt_save.pack(side=RIGHT, expand=True, fill=X)
+        self.et_save.pack(side=RIGHT, expand=True, fill=X, padx=5)
+        self.lb_save.pack(side=RIGHT)
+        
+        # linhas pack
+        self.line1.pack(fill=X)
+        self.line2.pack(anchor=CENTER,fill=BOTH, expand=True)
+        self.line3.pack(anchor=S,fill=X)
+        self.line4.pack(anchor=S,fill=X)
 
-        self.lb_save.grid(row=0, column=0, padx=5,)
-        self.et_save.grid(row=0, column=1, padx=5,)
-        self.bt_save.grid(row=0, column=3, padx=5,)
 
-        self.et_save.config(width=90)
-        self.bt_save.config(width=30)
-        self.fr_save.grid(row=4, column=1, sticky=EW, padx=5,)
         
         self.tt2.configure(state=DISABLED)
         # self.tt1.insert(1.0, 'hello world')
@@ -67,7 +70,6 @@ class Frame(ttk.Frame):
             defaultextension=".txt", 
             filetypes=[("All Files","*.*"),("Text Documents","*.txt")])
         
-        print(self.f)
     def cmd_open(self):
     
         self.filename = filedialog.askopenfilename(initialdir='./', 
@@ -92,12 +94,16 @@ class Frame(ttk.Frame):
 def main():
 
     window = ttk.Window()
-    window.style.theme_use('darkly')
+    window.style.theme_use('cyborg')
     # window.bind('q', lambda x: window.quit())
     window.bind('<Escape>', lambda x: window.quit())
-
+    width = window.winfo_screenwidth()
+    height = window.winfo_screenheight()
+    window.geometry(f'{width}x{height}')
     frame = Frame(window)
-    frame.pack()
+    frame.pack(fill=BOTH, expand=True)
+    frame.grid_columnconfigure(0, weight=1)
+    frame.grid_rowconfigure(0, weight=1)
     window.mainloop()
 
 if __name__ == "__main__":

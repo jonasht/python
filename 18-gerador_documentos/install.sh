1acf67f27034d749f48aa2b8dba9e44be9d5a18d
jonasht <jhenriquet@outlook.com.br>
2024-03-23 18:38:45 -0300

    fix install.sh

--- 18-gerador_documentos/install.sh
+++ 18-gerador_documentos/install.sh
@@ -1,4 +1,7 @@
 echo "funciona arch linux"
+echo "pode nao funcionar corretamento em outros OS"
+
+# xclip serve para pyperclip funcionar e tk serve para tkinter funcionar
 echo "instalando xclip e tk"
 sudo pacman -S xclip tk
 
@@ -7,3 +10,5 @@ python -m venv .env
 source .env/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "instalacao feita com sucesso" 

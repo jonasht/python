fb4d27eac3716fea0f7c6f379bd5434140df1a05
jonasht <jhenriquet@outlook.com.br>
2024-03-27 06:17:23 -0300

    add

--- 18-gerador_documentos/fr_faker.py
+++ 18-gerador_documentos/fr_faker.py
@@ -1,14 +1,69 @@
-import pyperclip as pc
+
+from tkinter import Misc
+from ttkbootstrap.constants import *
+import ttkbootstrap as ttk
+from ttkbootstrap import Window
 
 
-msg_str = 'o mundo vai acabar se o M0lusc0 ver as dicas do Peter (q fala que o bolsonaro ñ fez). e mandar o EXEs1to dar o G4LoP5, se ñ obedecer, "ele" manda embora '
-binary_converted = ''.join(format(ord(c), "b") for c in msg_str)
 
-# print(ord(msg_str))
-print(binary_converted)
-pc.copy(binary_converted)
+class Fr_faker(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-def decode_binary_string(s):
-    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
-X = msg_str
-print(chr(bin(X[:8])))
+        self.fr_left = ttk.Frame(self)
+        self.fr_right = ttk.Frame(self)
+
+        # frame left ==================
+        self.lb_nome = ttk.Label(self.fr_left, text='Nome:')
+        self.lb_fone = ttk.Label(self.fr_left, text='Telefone:')
+        self.lb_email = ttk.Label(self.fr_left, text='Email')
+        self.lb_dataNasc = ttk.Label(self.fr_left, text='Data Nascimento:')
+        self.et_nome = ttk.Entry(self.fr_left)
+        self.et_fone = ttk.Entry(self.fr_left)
+        self.et_email = ttk.Entry(self.fr_left)
+        self.et_dataNasc = ttk.Entry(self.fr_left)
+
+        # frame right ==================
+        self.lb_rua = ttk.Label(self.fr_right, text='Rua:')
+        self.lb_numero = ttk.Label(self.fr_right, text='Número:')
+        self.lb_bairro = ttk.Label(self.fr_right, text='Bairro:')
+        self.lb_cidade = ttk.Label(self.fr_right, text='Cidade:')
+        self.lb_estado = ttk.Label(self.fr_right, text='Estado:')
+        self.et_rua = ttk.Entry(self.fr_right)
+        self.et_numero = ttk.Entry(self.fr_right)
+        self.et_bairro = ttk.Entry(self.fr_right)
+        self.et_cidade = ttk.Entry(self.fr_right)
+        self.et_estado = ttk.Entry(self.fr_right)
+
+        # grid ========================
+        # frame left
+        self.lb_nome.grid(row=1, column=1)
+        self.lb_fone.grid(row=2, column=1)
+        self.lb_email.grid(row=3, column=1)
+        self.lb_dataNasc.grid(row=4, column=1)
+        self.et_nome.grid(row=1, column=2)
+        self.et_fone.grid(row=2, column=2)
+        self.et_email.grid(row=3, column=2)
+        self.et_dataNasc.grid(row=4, column=2)
+        # frame right
+        self.lb_rua.grid(row=1, column=1)
+        self.lb_numero.grid(row=2, column=1)
+        self.lb_bairro.grid(row=3, column=1)
+        self.lb_cidade.grid(row=4, column=1)
+        self.lb_estado.grid(row=5, column=1)
+        self.et_rua.grid(row=1, column=2)
+        self.et_numero.grid(row=2, column=2)
+        self.et_bairro.grid(row=3, column=2)
+        self.et_cidade.grid(row=4, column=2)
+        self.et_estado.grid(row=5, column=2)
+
+        self.fr_left.grid(row=1, column=1)
+        self.fr_right.grid(row=1, column=2)
+
+if __name__ == '__main__':
+    window = Window()
+    frame = Fr_faker(window)
+    frame.pack()
+    window.style.theme_use('darkly')
+
+    window.mainloop()
---
+++ 18-gerador_documentos/util_faker.py
@@ -0,0 +1,74 @@
+from faker import Faker
+from random import choice
+from datetime import datetime
+
+FAKE = Faker('pt_BR')
+
+
+
+class DadosPessoa:
+    emails_links = ['yahoo.com', 'yahoo.com.br',
+                        'outlook.com', 'outlook.com.br',
+                        'hotmail.com', 'hotmail.com.br',
+                        'gmail.com', 'icloud.com', 'aol.com',
+                        'live.com', 'bol.com.br', 'uol.com.br',
+                        'terra.com.br', 'ig.com.br',
+                        'r7.com', 'zipmail.com.br',
+                        'protonmail.com','yandex.com']
+    def __init__(self) -> None:
+        
+        self.generate()
+    
+
+    def generate(self) -> None:
+        def create_email(nome:str) -> str:
+            return nome.replace(' ', '') +'@' + choice(self.emails_links)
+        
+        self.nome = FAKE.name()
+        self.fone = FAKE.phone_number()
+        self.email = create_email(self.nome)
+        self.data_nascimento = FAKE.date_of_birth(minimum_age=18, maximum_age=70, ).strftime("%d/%m/%Y")
+    
+
+
+    def show(self) -> print:
+        def l(qtd=30, oq='='):
+            print(oq*qtd)
+        l()
+        print('              nome:', self.nome)
+        print('          telefone:', self.fone)
+        print('             email:', self.email)
+        print('data de nascimento:', self.data_nascimento)
+        l()
+
+
+
+
+class DadosLocal:
+    def __init__(self) -> None:
+        self.generate()
+
+    def generate(self):
+        
+        self.rua = FAKE.street_name()
+        self.numero = FAKE.building_number()
+        self.bairro = FAKE.neighborhood()
+        self.cidade = FAKE.city()
+        self.estado = FAKE.state_abbr()     
+
+    def show(self):
+        def l(qtd=30, oq='='):
+            print(oq*qtd)
+        l()
+        print("   Rua:", self.rua)
+        print("Número:", self.numero)
+        print("Bairro:", self.bairro)
+        print("Cidade:", self.cidade)
+        print("Estado:", self.estado)
+        l()
+
+dadosP = DadosPessoa()
+dadosL = DadosLocal()
+
+dadosP.show()
+dadosL.show()
